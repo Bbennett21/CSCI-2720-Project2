2abffd427d6eaad307b919fd09a52abc791b1a8e
Bryson Bennett <blb64218@uga.edu>
2023-02-21 11:52:15 -0500

    created outline and implemented driver

---
+++ LinkedListStack.java
@@ -0,0 +1,49 @@
+public class LinkedListStack<T> {
+    private Node<T> top;
+
+    private class Node<T> {
+        T value;
+        Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+            next = null;
+        }
+    }
+
+    public void push(T value) {
+        Node<T> newNode = new Node<>(value);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public T pop() {
+        if (top == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        T poppedValue = top.value;
+        top = top.next;
+        return poppedValue;
+    }
+
+    public T peek() {
+        if (top == null) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return top.value;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public int size() {
+        int count = 0;
+        Node<T> current = top;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+}
