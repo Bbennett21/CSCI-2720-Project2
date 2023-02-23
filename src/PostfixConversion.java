ef735255385ac7ef906f7780619a5b89916b9523
Bryson Bennett <blb64218@uga.edu>
2023-02-22 23:12:31 -0500

    fixed expression validation

--- src/PostfixConversion.java
+++ src/PostfixConversion.java
@@ -1,71 +1,85 @@
 public class PostfixConversion {
     public static boolean isValidInfixExpression(String infixExpression) {
-        LinkedListStack stack = new LinkedListStack();
-        for (int i = 0; i < infixExpression.length(); i++) {
-            char c = infixExpression.charAt(i);
-            if (c == '(') {
-                stack.push(c);
-            } else if (c == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                } // if
-                char top = stack.pop();
-                if (c == ')' && top != '(') {
-                    return false;
-                } // if
-            } else if (Character.isDigit(c)) {
-                // Makes sure a digit is followed by an operator or closing bracket
-                int j = i + 1;
-                while (j < infixExpression.length() && Character.isWhitespace(infixExpression.charAt(j))) {
-                    j++; // skip whitespace
-                } // while
-                if (j < infixExpression.length() - 1) {
-                    char next = infixExpression.charAt(j + 1);
-                    if (Character.isDigit(next)) {
-                        return false;
-                    } // if
-                } // if
-            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                // If current character is an operator, make sure it is surrounded by digits
-                if (i == 0 || i == infixExpression.length() - 1) {
-                    return false;
-                } // if
-                int j = i - 1;
-                while (j >= 0 && Character.isWhitespace(infixExpression.charAt(j))) {
-                    j--; // skip whitespace
-                } // while
-                if (j < 0 || !Character.isDigit(infixExpression.charAt(j))) {
-                    return false;
-                } // if
-                j = i + 1;
-                while (j < infixExpression.length() && Character.isWhitespace(infixExpression.charAt(j))) {
-                    j++; // skip whitespace
-                } // while
-                if (j >= infixExpression.length() || !Character.isDigit(infixExpression.charAt(j))) {
-                    return false;
-                } // if
-            } else if (c == ' ') {
-                continue;
-            } else {
-                return false; // Invalid character found
+    LinkedListStack stack = new LinkedListStack();
+    boolean prevWasDigit = false; // flag to keep track of whether previous character was a digit
+    for (int i = 0; i < infixExpression.length(); i++) {
+        char c = infixExpression.charAt(i);
+        if (c == ' ') {
+            continue; // skip whitespace
+        } else if (c == '(') {
+            stack.push(c);
+            prevWasDigit = false; // reset flag
+        } else if (c == ')') {
+            if (stack.isEmpty()) {
+                return false;
             } // if
-        } // for
-        return stack.isEmpty();
-    } // isValidInfixExpression
+            char top = stack.pop();
+            if (c == ')' && top != '(') {
+                return false;
+            } // if
+            prevWasDigit = false; // reset flag
+        } else if (Character.isDigit(c)) {
+            // Makes sure a digit is part of a single number
+            if (prevWasDigit) {
+                continue; // digit is part of same number
+            } // if
+            int j = i + 1;
+            while (j < infixExpression.length() && Character.isWhitespace(infixExpression.charAt(j))) {
+                j++; // skip whitespace
+            } // while
+            if (j < infixExpression.length() && Character.isDigit(infixExpression.charAt(j))) {
+                prevWasDigit = true; // digit is part of same number
+            } else {
+                prevWasDigit = false; // digit is not part of same number
+            } // if
+        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+            // If current character is an operator, make sure it is surrounded by digits
+            if (i == 0 || i == infixExpression.length() - 1) {
+                return false;
+            } // if
+            int j = i - 1;
+            while (j >= 0 && Character.isWhitespace(infixExpression.charAt(j))) {
+                j--; // skip whitespace
+            } // while
+            if (j < 0 || !Character.isDigit(infixExpression.charAt(j))) {
+                return false;
+            } // if
+            j = i + 1;
+            while (j < infixExpression.length() && Character.isWhitespace(infixExpression.charAt(j))) {
+                j++; // skip whitespace
+            } // while
+            if (j >= infixExpression.length() || !Character.isDigit(infixExpression.charAt(j))) {
+                return false;
+            } // if
+            prevWasDigit = false; // reset flag
+        } else {
+            return false; // Invalid character found
+        } // if
+    } // for
+    return stack.isEmpty();
+} // isValidInfixExpression
+
 
     public static String convertToPostfix(String infix) throws IllegalArgumentException {
         LinkedListStack stack = new LinkedListStack();
         String postfix = "";
+        String number = "";
         for (char c : infix.toCharArray()) {
             if (c == ' ') {
                 continue;
-            } // if
-            if (Character.isLetterOrDigit(c)) {
+            } else if (Character.isDigit(c)) {
+                number += c;
+            } else if (Character.isLetter(c)) {
                 postfix += c;
                 postfix += " ";
             } else if (c == '(') {
                 stack.push(c);
             } else if (c == ')') {
+                if (!number.equals("")) {
+                    postfix += number;
+                    postfix += " ";
+                    number = "";
+                }
                 while (!stack.isEmpty() && stack.peek() != '(') {
                     postfix += stack.pop();
                     postfix += " ";
@@ -75,6 +89,11 @@ public class PostfixConversion {
                 } // if
                 stack.pop(); // Pop the left parenthesis
             } else {
+                if (!number.equals("")) {
+                    postfix += number;
+                    postfix += " ";
+                    number = "";
+                }
                 while (!stack.isEmpty() && getPrecedence(c) <= getPrecedence(stack.peek())) {
                     postfix += stack.pop();
                     postfix += " ";
@@ -82,6 +101,10 @@ public class PostfixConversion {
                 stack.push(c);
             } // if
         } // for
+        if (!number.equals("")) {
+            postfix += number;
+            postfix += " ";
+        }
         while (!stack.isEmpty()) {
             if (stack.peek() == '(' || stack.peek() == ')') {
                 throw new IllegalArgumentException("Unbalanced parentheses.");
@@ -91,6 +114,7 @@ public class PostfixConversion {
         } // while
         return postfix;
     } // convertToPostfix
+
 
     private static int getPrecedence(char c) {
         if (c == '+' || c == '-') {
