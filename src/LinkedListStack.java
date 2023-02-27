cc8287cce2dfb966958501399dc5c17c67721eed
Bryson Bennett <blb64218@uga.edu>
2023-02-26 20:36:00 -0500

    added comments

--- src/LinkedListStack.java
+++ src/LinkedListStack.java
@@ -1,6 +1,11 @@
+
 public class LinkedListStack<T> {
+
     private Node<T> top;
 
+    /**
+     * Generic Node class creates generic nodes.
+     */
     private static class Node<T> {
         T value;
         Node<T> next;
@@ -11,12 +16,19 @@ public class LinkedListStack<T> {
         } // Node
     } // Node<T>
 
+    /**
+     * Push pushes a value onto the stack.
+     * @param is the value to be pushed.
+     */
     public void push(T value) {
         Node<T> newNode = new Node<>(value);
         newNode.next = top;
         top = newNode;
     } // push
 
+    /**
+     * Pop returns the value at the top of the stack and removes it from the stack.
+     */
     public T pop() {
         if (top == null) {
             throw new IllegalStateException("Stack is empty");
@@ -26,6 +38,9 @@ public class LinkedListStack<T> {
         return poppedValue;
     } // pop
 
+    /**
+     * Peek returns the value at the top of the stack without removing it.
+     */
     public T peek() {
         if (top == null) {
             throw new IllegalStateException("Stack is empty");
@@ -33,10 +48,16 @@ public class LinkedListStack<T> {
         return top.value;
     } // peek
 
+    /**
+     * IS Empty checks if the stack is empty.
+     */
     public boolean isEmpty() {
         return top == null;
     } // isEmpty
 
+    /**
+     * size returns the size of the stack.
+     */
     public int size() {
         int count = 0;
         Node<T> current = top;
